aad74a2c1e4323e599136620d6cd598fffe2af3f
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-11 13:15:38 +0200

    Add instances of defined classes

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import Personaje from './Personaje.js';
 import Rey from './Rey.js';
+import Escuder from './Escudero.js';
+import Luchador from "./Luchador.js";
+import Asesor from './Asesor.js';
 
+const joffrey = new Rey("Joffrey Baratheon");
+const jaime = new Luchador("Jaime Lannister");
+const daenerys = new Luchador("Daenerys Targaryen");
+const tyrion = new Asesor("Tyrion Lannister",daenerys);
+const bronn =  new Escuder("Bronn", jaime);
 
+const personajeList = [joffrey, jaime, daenerys, tyrion, bronn];
